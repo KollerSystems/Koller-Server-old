a43377e9941bcf4c72b5965332aea227aeb96d79
IronNight007 <113416142+IronNight007@users.noreply.github.com>
2023-02-26 22:07:26 +0100

    Add files via upload
    with test data(without pictures)

--- database/kollegium.sql
+++ database/kollegium.sql
@@ -1,3 +1,10 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Szerver verzió:               10.10.3-MariaDB - mariadb.org binary distribution
+-- Szerver OS:                   Win64
+-- HeidiSQL Verzió:              11.3.0.6295
+-- --------------------------------------------------------
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
@@ -5,30 +12,12 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATABASE IF NOT EXISTS `kollegium` /*!40100 DEFAULT CHARACTER SET utf8mb3 */;
+
+-- Adatbázis struktúra mentése a kollegium.
+CREATE DATABASE IF NOT EXISTS `kollegium` /*!40100 DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci */;
 USE `kollegium`;
 
-CREATE TABLE IF NOT EXISTS `crossings` (
-  `ID` int(15) unsigned NOT NULL AUTO_INCREMENT,
-  `SID` int(15) unsigned DEFAULT NULL,
-  `Time` datetime DEFAULT NULL,
-  `Direction` binary(2) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-/*!40000 ALTER TABLE `crossings` DISABLE KEYS */;
-/*!40000 ALTER TABLE `crossings` ENABLE KEYS */;
-
-CREATE TABLE IF NOT EXISTS `login_data` (
-  `GID` int(20) unsigned NOT NULL,
-  `Username` text DEFAULT NULL,
-  `Password` text DEFAULT NULL,
-  PRIMARY KEY (`GID`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-/*!40000 ALTER TABLE `login_data` DISABLE KEYS */;
-/*!40000 ALTER TABLE `login_data` ENABLE KEYS */;
-
+-- Struktúra mentése tábla kollegium. auth
 CREATE TABLE IF NOT EXISTS `auth` (
   `ID` varchar(12) DEFAULT NULL,
   `access_token` varchar(32) DEFAULT NULL,
@@ -38,7 +27,41 @@ CREATE TABLE IF NOT EXISTS `auth` (
   `expired` bit(1) NOT NULL DEFAULT b'0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
+-- Tábla adatainak mentése kollegium.auth: ~0 rows (hozzávetőleg)
+/*!40000 ALTER TABLE `auth` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth` ENABLE KEYS */;
 
+-- Struktúra mentése tábla kollegium. crossings
+CREATE TABLE IF NOT EXISTS `crossings` (
+  `ID` int(15) unsigned NOT NULL AUTO_INCREMENT,
+  `SID` int(15) unsigned DEFAULT NULL,
+  `Time` datetime DEFAULT NULL,
+  `Direction` binary(2) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Tábla adatainak mentése kollegium.crossings: ~0 rows (hozzávetőleg)
+/*!40000 ALTER TABLE `crossings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `crossings` ENABLE KEYS */;
+
+-- Struktúra mentése tábla kollegium. login_data
+CREATE TABLE IF NOT EXISTS `login_data` (
+  `GID` int(20) unsigned NOT NULL,
+  `Username` text DEFAULT NULL,
+  `Password` text DEFAULT NULL,
+  PRIMARY KEY (`GID`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Tábla adatainak mentése kollegium.login_data: ~0 rows (hozzávetőleg)
+/*!40000 ALTER TABLE `login_data` DISABLE KEYS */;
+INSERT INTO `login_data` (`GID`, `Username`, `Password`) VALUES
+	(1, 'Miki', 'almafa1234'),
+	(2, 'Barnabás', 'almafa1234'),
+	(3, 'Gyuri', 'almafa1234'),
+	(4, 'Gergő', 'almafa1234');
+/*!40000 ALTER TABLE `login_data` ENABLE KEYS */;
+
+-- Struktúra mentése tábla kollegium. peroutgoing
 CREATE TABLE IF NOT EXISTS `peroutgoing` (
   `SID` int(10) unsigned NOT NULL DEFAULT 0,
   `TID` int(10) unsigned DEFAULT NULL,
@@ -46,11 +69,13 @@ CREATE TABLE IF NOT EXISTS `peroutgoing` (
   `StartTime` time DEFAULT NULL,
   `EndTime` time DEFAULT NULL,
   KEY `Index 1` (`SID`,`StartTime`,`EndTime`,`Day`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
+-- Tábla adatainak mentése kollegium.peroutgoing: ~0 rows (hozzávetőleg)
 /*!40000 ALTER TABLE `peroutgoing` DISABLE KEYS */;
 /*!40000 ALTER TABLE `peroutgoing` ENABLE KEYS */;
 
+-- Struktúra mentése tábla kollegium. roomorder
 CREATE TABLE IF NOT EXISTS `roomorder` (
   `ID` int(15) NOT NULL AUTO_INCREMENT,
   `TID` int(11) unsigned DEFAULT NULL,
@@ -68,11 +93,13 @@ CREATE TABLE IF NOT EXISTS `roomorder` (
   `Tidiness` tinyint(4) unsigned DEFAULT NULL,
   `Unwashed` binary(2) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
+-- Tábla adatainak mentése kollegium.roomorder: ~0 rows (hozzávetőleg)
 /*!40000 ALTER TABLE `roomorder` DISABLE KEYS */;
 /*!40000 ALTER TABLE `roomorder` ENABLE KEYS */;
 
+-- Struktúra mentése tábla kollegium. student
 CREATE TABLE IF NOT EXISTS `student` (
   `ID` int(15) unsigned NOT NULL AUTO_INCREMENT,
   `OM` varchar(11) DEFAULT NULL,
@@ -83,60 +110,57 @@ CREATE TABLE IF NOT EXISTS `student` (
   `School` text DEFAULT NULL,
   `Birthplace` text DEFAULT NULL,
   `Birthdate` date DEFAULT NULL,
-  `GuardiaName` text DEFAULT NULL,
-  `GuardianPhone` int(11) DEFAULT NULL,
-  `Address` text DEFAULT NULL,
-  `RoomNumber` tinyint(4) DEFAULT NULL,
+  `GuardianName` text DEFAULT NULL,
+  `GuardianPhone` varchar(50) DEFAULT NULL,
+  `RoomNumber` smallint(5) unsigned DEFAULT NULL,
+  `Country` text DEFAULT NULL,
+  `City` text DEFAULT NULL,
+  `Street` text DEFAULT NULL,
+  `PostCode` int(11) unsigned DEFAULT NULL,
+  `Address` int(10) unsigned DEFAULT NULL,
+  `Floor` int(11) unsigned DEFAULT NULL,
+  `Door` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`ID`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=4294967296 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
+-- Tábla adatainak mentése kollegium.student: ~4 rows (hozzávetőleg)
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` (`ID`, `OM`, `Name`, `Picture`, `Group`, `Class`, `School`, `Birthplace`, `Birthdate`, `GuardianName`, `GuardianPhone`, `RoomNumber`, `Country`, `City`, `Street`, `PostCode`, `Address`, `Floor`, `Door`) VALUES
+	(1, '73454685362', 'Várnagy Miklós', NULL, 'F8', '11.B', 'BMSZC Puskás Tivadar Távközlési Technikum', 'Budapest', '2023-02-26', 'Papp Lajos', '36 64 865 3423', 126, 'Új-Zéland', 'Hamilton', 'Clarkin Road', 3214, 2, NULL, NULL),
+	(2, '72745678344', 'Katona Márton Barnabás', NULL, 'F10', '11.B', 'BMSZC Puskás Tivadar Távközlési Technikum', 'Budapest', '2005-07-22', 'Kis Gazsiné', '213 676 33 87 93', 32, 'Afganistan', 'Kabul', 'Asmayi Road', 553, 8, 3, NULL),
+	(3, '72345456668', 'Bencsik Gergely', NULL, 'F3', '11.B', 'BMSZC Puskás Tivadar Távközlési Technikum', 'Budapest', '2004-02-28', 'Tóth András', '36 90 343 5454', 535, 'Uganda', 'Kampala', 'Kabalega Close', NULL, 16, NULL, NULL),
+	(4, '74583725375', 'Bende Ákos György', NULL, 'L1', '11.B', 'BMSZC Puskás Tivadar Távközlési Technikum', 'Budapest', '2005-08-15', 'Kalapos József', '43 673 527890180', 264, 'Austria', 'Graz', 'Plüddemanngasse', 8010, 62, 32, 3);
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 
+-- Struktúra mentése tábla kollegium. teacher
 CREATE TABLE IF NOT EXISTS `teacher` (
   `ID` int(15) unsigned NOT NULL AUTO_INCREMENT,
   `Name` text DEFAULT NULL,
   `OM` varchar(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
+-- Tábla adatainak mentése kollegium.teacher: ~0 rows (hozzávetőleg)
 /*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
 /*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
 
+-- Struktúra mentése tábla kollegium. user
 CREATE TABLE IF NOT EXISTS `user` (
   `GID` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `ID` int(15) unsigned DEFAULT NULL,
   `Role` tinyint(1) unsigned DEFAULT 0,
   PRIMARY KEY (`GID`),
   KEY `ID` (`ID`,`Role`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
+-- Tábla adatainak mentése kollegium.user: ~0 rows (hozzávetőleg)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`GID`, `ID`, `Role`) VALUES
+	(1, 1, 0),
+	(2, 2, 0),
+	(3, 3, 0),
+	(4, 4, 0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
-
-CREATE TABLE IF NOT EXISTS `role_name` (
-  `Role` tinyint(1) unsigned DEFAULT 0,
-  `Table` varchar(64) NOT NULL,
-  `FullName` text DEFAULT NULL,
-  PRIMARY KEY (`Role`),
-  KEY (`Table`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-INSERT INTO `role_name` (`Role`, `Table`, `FullName`) VALUES
-  (1, 'student', 'kollégiumi diák');
-INSERT INTO `role_name` (`Role`, `Table`, `FullName`) VALUES
-  (2, 'teacher', 'nevelőtanár');
-
-/*!40000 ALTER TABLE `role_name` DISABLE KEYS */;
-/*!40000 ALTER TABLE `role_name` ENABLE KEYS */;
-
-
-INSERT INTO `student` (`ID`, `OM`, `Name`, `Picture`, `Group`, `Class`, `School`, `Birthplace`, `Birthdate`, `GuardiaName`, `GuardianPhone`, `Address`, `RoomNumber`) VALUES
-	(32, '72136781423', 'Pista', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `user` (`ID`, `Role`) VALUES
-  (32,1);
-INSERT INTO `login_data` (`GID`, `Username`, `Password`) VALUES
-  (1, 'Xx_Pistike_xX', 'GoofyEmber');
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
