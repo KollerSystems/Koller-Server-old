729dc237d0801211839011ee9af029f40aa1024d
lolfail <bencsikgergo08@gmail.com>
2023-09-14 19:29:37 +0200

    ✨🧐benchmark opció test script-eknél

--- api/test/oauth.js
+++ api/test/oauth.js
@@ -1,4 +1,5 @@
 import { readFile } from 'fs/promises';
+import { readFileSync, writeFileSync } from 'fs';
 const options = JSON.parse(
   await readFile(
     new URL('../options.json', import.meta.url)
@@ -16,6 +17,42 @@ import { expect } from 'chai';
 
 
 describe('Getting token & refreshing it with various credentials', function () {
+  let testN = 0;
+  let tests = [];
+  beforeEach(function () {
+    tests[testN] = { startT: new Date() };
+  });
+
+  afterEach(function () {
+    tests[testN].time = new Date() - tests[testN++].startT;
+  });
+  after(async function () {
+    const curTest = 'oauth';
+    const benchmark = parameters.benchmark;
+    if (!benchmark.on) return;
+    let file = readFileSync(benchmark.file).toString().split('\n');
+    if (file[0].split(benchmark.delimiter).length == 1) {
+      file[0] = [ '"test"', '"min"', '"avg"', '"max"' ].join(benchmark.delimiter);
+      file.push('');
+      writeFileSync(benchmark.file, file[0] + '\n');
+    }
+    let current = file.at(-1).split(benchmark.delimiter);
+    if (current.length == 1) for (let i = 1; i < 4; i++) current.push('');
+
+    current[0] = '"' + curTest + '"';
+    current[1] = tests.sort((a, b) => a.time - b.time)[0].time;
+    let sum = 0;
+    tests.forEach(o => sum += o.time);
+    current[2] = sum/tests.length;
+    current[3] = tests.sort((a, b) => b.time - a.time)[0].time;
+
+    current = current.join(benchmark.delimiter);
+    file[file.length-1] = current;
+    file = file.join('\n');
+    writeFileSync(benchmark.file, file + '\n');
+  });
+
+
   const user = (() => {
     for (let parameter of parameters.oauth) if (parameter.succeed) {
       return parameter.credentials;
